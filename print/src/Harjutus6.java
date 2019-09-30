7a00d893e6ff1becf6c804bc31551bc36312d146
steven.kore <steven.kore@khk.ee>
2019-09-30 11:21:05 +0300

    Java addition and multiplication.

---
+++ print/src/Harjutus6.java
@@ -0,0 +1,8 @@
+public class Harjutus6 {
+        public static void main(String[] args) {
+            int x = 5;
+            int y = 4;
+            int sum = x + y;
+            System.out.println(x + " + " + y + " = " + sum);
+        }
+    }
---
+++ print/src/Harjutus7.java
@@ -0,0 +1,8 @@
+public class Harjutus7 {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 8;
+        int sum = x * y;
+        System.out.println(x + " * " + y + " = " + sum);
+    }
+}
