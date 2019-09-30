a7eb76f46d472bc50f622c9ce3f91dc4ffdfbfe7
steven.kore <steven.kore@khk.ee>
2019-09-30 11:04:26 +0300

    Algus java.

---
+++ print/src/Harjutus5.java
@@ -0,0 +1,14 @@
+public class Harjutus5 {
+    public static void main(String[] args) {
+
+        int days = 365;
+        int hours = 24;
+        int minutes = 60;
+        int seconds = 60;
+
+
+        int secondInAYear = days * hours * minutes * seconds;
+        System.out.println("There are " + secondInAYear + " seconds in a year.");
+
+    }
+}
