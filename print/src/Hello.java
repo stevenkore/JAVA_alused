02ed27a6944b56d10e46732c8ffdf3a250748b9e
steven.kore <steven.kore@khk.ee>
2019-09-30 10:33:59 +0300

    Algus java.

---
+++ print/src/Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("Hello World!\n");
+        System.out.print("(and all the people of the world.)\n");
+        System.out.print("     * \n    *** \n   ***** \n  *******\n ********* \n     *\n");
+
+
+
+        int wholeNumber = 3;
+        double decimalNumber = 5.5;
+        String text = "There is none!";
+
+        System.out.println("Chickens:\n" + wholeNumber);
+        System.out.println("Bacon (kg):\n" + decimalNumber);
+        System.out.println("A tractor:\n" + text);
+    }
+}
+
